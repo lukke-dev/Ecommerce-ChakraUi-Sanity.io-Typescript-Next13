a8d0737a7bf8489202c816f66bf1a186b8096ff6
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-08-24 00:04:15 -0300

    ✨ feat(Create Structure to categories and products page, and add loading to them):

---
+++ src/app/(user)/products/loading.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { NextPage } from 'next'
+import { Loading } from '@src/components'
+
+const LoadingPage: NextPage = () => {
+  return <Loading />
+}
+
+export default LoadingPage
---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import { Flex, Text } from '@chakra-ui/react'
+
+export const Loading: React.FC = () => {
+  return (
+    <Flex
+      minH="calc(100vh - 150px)"
+      justify="center"
+      align="center"
+      flexDir="column"
+    >
+      <Image
+        src="/loading-cart.gif"
+        alt="loading cart"
+        width={200}
+        height={200}
+      />
+      <Text>Loading...</Text>
+    </Flex>
+  )
+}
