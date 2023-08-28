a204113e7ea82868b03ffb7648e110a7220b94e5
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-08-27 21:06:51 -0300

    🐞 fix(Fix eslint error):

--- src/pages/home/components/banner/banner.tsx
+++ src/pages/home/components/banner/banner.tsx
@@ -33,7 +33,7 @@ export const Banner: React.FC = () => {
               color="white"
               borderRadius="50px"
               bgColor="pink.500"
-              _hover={{ bgColor: "pink.600" }}
+              _hover={{ bgColor: 'pink.600' }}
             >
               Shop Now
             </Button>
